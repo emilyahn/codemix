897b0ff450f1d53ee8e0a3dcb7b7abdf069b64c2
Emily Ahn <eahn@wellesley.edu>
2018-07-26 12:23:21 -0400

    calc styles across miami, twitter, cocoa

---
+++ cm-calc-notes.md
@@ -0,0 +1,33 @@
+### MIAMI
+
+with PRON in function class
+c-0-s	24.18
+c-0-w	16.40
+c-1-s	5.04
+c-1-w	11.11
+neither	31.01
+s-0		6.54
+s-1		5.72
+
+### TWITTER
+perhaps reduce constraint on FUNC 2+?
+
+c-0-s	19.37
+c-0-w	29.34
+c-1-s	4.11
+c-1-w	17.72
+neither	20.07
+s-0		3.87
+s-1		5.52
+
+
+### (other experimental variations)
+MIAMI
+without PRON in function class
+c-0-s	27.55
+c-0-w	16.87
+c-1-s	4.61
+c-1-w	10.17
+neither	35.41
+s-0		3.42
+s-1		1.96
--- src/cm_metrics.py
+++ src/cm_metrics.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # from src.parse_miami import load_data, load_spkr_info
 import spacy
-# from itertools import groupby
+from itertools import groupby
 from collections import defaultdict
+import math
 
 
 nlp_en = spacy.load('en')
@@ -28,9 +29,10 @@ def remove_lidtags_miami(text):
 
 # take full text (list of str), return [spa_closed_lst, eng_closed_lst]
 # each sublist contains Booleans
-def get_closed(text, spa_pos, eng_pos):
-	closed_class = ['AUX', 'DET', 'CONJ', 'CCONJ', 'ADP']
-	text = remove_lidtags_miami(text)
+# TODO: try adding PRON, removing DET
+def get_func(text, spa_pos, eng_pos):
+	closed_class = ['AUX', 'CONJ', 'CCONJ', 'ADP', 'PRON']
+	# text = remove_lidtags_miami(text)
 	# get english
 	eng_closed_lst = []
 	for i, word in enumerate(text):
@@ -38,8 +40,6 @@ def get_closed(text, spa_pos, eng_pos):
 		if word in eng_closed_lst or eng_pos[i] in closed_class:
 			eng_is_closed = True
 		eng_closed_lst.append(eng_is_closed)
-
-	# eng_aux = [word in eng_aux_lst for word in text]
 
 	# get spanish
 	# doc = nlp_all[0](unicode(' '.join(text), "utf-8"))
@@ -62,64 +62,159 @@ def get_pos(text):
 # given list of POS tags, return Boolean list of if it's NOUN/ADJ/VERB
 # tag set inspiration: Content morphemes in Myers-Scotton's ML/EL framework
 # noise: English aux can be tagged as VERB, e.g. "is"
-# TODO: try removing PROPN, and adding PRON
-def is_content(pos_lst):
-	# cont_lst = []
-	# for pos_tag in pos_lst:
-	# 	if pos_tag in ['NOUN', 'ADJ']:
-	# 		cont_lst.append('1')
-	# 	if pos_tag == 'VERB'
-	# 		if
+# TODO: try removing PROPN
+def get_content(pos_lst):
 	return [pos_tag in ['NOUN', 'ADJ', 'VERB', 'PROPN'] for pos_tag in pos_lst]
 
 
-# split into 2 methods later: miami-specific, then style metrics
-def get_style_metrics(all_data, dialog_id):
-	# total_turns = sum([len(
-	# 					all_data[dialog_id][spkr]['turn_num']
-	# 					) for spkr in all_data[dialog_id].keys()])
+# check if (1) Structure
+# return one of {None, 0, 1}
+# rule: 2 consecutive L1 func, and 2 consecutive L2 func -- in same utt
+def is_structure(words_01, both_aux):
+	func_lst = []  # 0s (ENG) and 1s (SPA) of function words only
+	for i in range(len(words_01)):
+		lang = words_01[i]
+		if lang not in [0, 1]:
+			continue
+		if both_aux[lang][i]:  # this word is an AUX in lang X
+			func_lst.append(lang)
 
-	for spkr in all_data[dialog_id].keys():
-		for line_i, words_01 in enumerate(all_data[dialog_id][spkr]['words_01']):
-			# only process if utt = code-mixed
-			if not (0 in words_01 and 1 in words_01):
-				continue
+	# TESTING ONLY
+	# func_lst = [0, 1, 0, 0, 1, 1, 0, 1, 0]
 
-			words_lst = remove_lidtags_miami(all_data[dialog_id][spkr]['words'][line_i])
-			print ' '.join(words_lst)
-			spa_pos, eng_pos = get_pos(words_lst)
-			spa_aux, eng_aux = get_closed(words_lst, spa_pos, eng_pos)
-			spa_cont = is_content(spa_pos)
-			eng_cont = is_content(eng_pos)
+	# get list of (key, num-consecutive):
+	# EX: [ (0, 2), (1, 3), ... ] for [0,0,1,1,1,...]
+	num_func_groups = [(k, len(list(g))) for k, g in groupby(func_lst)]
 
-			for i in range(len(words_01)):
-				print '{}: {}'.format(words_01[i], words_lst[i])
-				print 'S-POS\t{}\tE-POS\t{}'.format(spa_pos[i], eng_pos[i])
-				print 'S-AUX\t{}\tE-AUX\t{}'.format(int(spa_aux[i]), int(eng_aux[i]))
-				print 'S-CON\t{}\tE-CON\t{}'.format(int(spa_cont[i]), int(eng_cont[i]))
-				print ''
-			# print 'spa_pos', spa_pos
-			# print 'eng_pos', eng_pos
+	atleast2 = [False, False]
+	first_atleast2 = None
+	for lang, num_consec in num_func_groups:
+		# if num_consec >= 2:
+		if num_consec >= 1:
+			atleast2[lang] = True
+			# assign 0 or 1, for which lang first has >=2 # of func
+			if first_atleast2 is None:
+				first_atleast2 = lang
 
-			# # spans: [ (0, 2), (1, 3), ... ] for [0,0,1,1,1,...]
-			# spans = [(k, len(list(g))) for k, g in groupby(line)]
-			# word_idx = 0
-			# for span in spans:
-			# 	words_str = ''  # UNICODE ERRORS?
-			# 	for this_word_idx in range(span[1]):
-			# 		this_word = all_data[dialog_id][spkr]['words'][line_i][word_idx].split('_')[0]
-			# 		words_str += ' {}'.format(this_word)
-			# 		word_idx += 1
+	# print 'FUNC LIST', num_func_groups
+	# print 'atleast2', atleast2
+	# print 'first_atleast2', first_atleast2
 
-			# 	words_str = words_str.strip()
-			# 	print words_str
-			# 	# span[0] = {0,1}
-			# 	doc = nlp_all[span[0]](unicode(words_str, "utf-8"))
-			# 	for token in doc:
-			# 		print token.pos_
-			# 		if token.pos_ == 'NOUN':
-			# 			pass
-			break
+	if all(atleast2):
+		return first_atleast2
+
+	return None
+
+
+# check if (2) Content
+# return None or (0|1, 'w'|'s')
+# 0 indicates matrix lang
+# 'w' (weak) : num L2 content > 0
+# 's' (strong) : (weak) && num L2 content >= num L1 content
+def is_content(words_01, both_aux, both_cont):
+	num_aux = [0, 0]
+	num_cont = [0, 0]
+	# accumulate number of function and content words per language
+	for i in range(len(words_01)):
+		lang = words_01[i]
+		if lang not in [0, 1]:
+			continue
+		num_aux[lang] += int(both_aux[lang][i])
+		num_cont[lang] += int(both_cont[lang][i])
+
+	# print 'AUX:', num_aux
+	# print 'CONT:', num_cont
+	winner = None
+	if num_aux[0] > num_aux[1]:
+		winner = 0
+	elif num_aux[1] > num_aux[0]:
+		winner = 1
+
+	if winner is None:
+		return None
+	else:
+		strength = None
+		if num_cont[winner - 1] > 0:
+			strength = 'w'
+		if num_cont[winner - 1] >= num_cont[winner]:
+			strength = 's'
+
+		if strength is not None:
+			return (winner, strength)
+		else:
+			return None
+
+
+# from styles dict, compute proportions of each style
+def calc_styles(styles):
+	total_utt = float(sum([v for k, v in styles.iteritems()]))
+	sorted_names = sorted(styles.keys())
+	for style_name in sorted_names:
+		count = styles[style_name]
+		print '{}\t{:.2f}'.format(style_name, (100 * count)/total_utt)
+
+
+# give an utterance's LID labels and list(text)
+# update styles and styles_txt dictionaries
+def process_tags(words_01, words_lst, styles, styles_txt):
+	# all indices -> 0 = SPA, 1 = ENG
+	both_pos = get_pos(words_lst)
+	spa_pos, eng_pos = both_pos
+
+	both_aux = get_func(words_lst, spa_pos, eng_pos)
+	both_cont = [get_content(spa_pos), get_content(eng_pos)]
+
+	structure = is_structure(words_01, both_aux)
+	# print 'STRUCTURE?', structure
+
+	content = is_content(words_01, both_aux, both_cont)
+	# print 'CONTENT?', content
+
+	# check if both styles are present
+	# if structure is not None and content is not None:
+	# 	print '*'*20
+	# 	print 's-{}'.format(structure)
+	# 	print 'c-{}-{}'.format(content[0], content[1])
+
+	name = ''
+	if structure is not None:
+		name = 's-{}'.format(structure)
+	elif content is not None:
+		name = 'c-{}-{}'.format(content[0], content[1])
+	else:
+		name = 'neither'
+
+	styles[name] += 1
+	styles_txt[name].append(' '.join(words_lst))
+
+
+# print CM metrics for Miami data only
+# return dict where with {style: list(txt examples)}
+def get_style_metrics_miami(all_data):
+	styles = defaultdict(int)
+	styles_txt = defaultdict(list)
+	for dialog_id in all_data.keys():
+		# dialog_id = 'zeledon8'  # DEBUGGING
+		num_cm_utt = 0
+		for spkr in all_data[dialog_id].keys():
+			# spkr = 'MAR'  # DEBUGGING
+			for line_i, words_01 in enumerate(all_data[dialog_id][spkr]['words_01']):
+				# only process if utt = code-mixed
+				if not (0 in words_01 and 1 in words_01):
+					continue
+
+				num_cm_utt += 1
+				words_lst = remove_lidtags_miami(all_data[dialog_id][spkr]['words'][line_i])
+				if words_lst[-1] == '':
+					words_lst = words_lst[:-1]
+
+				process_tags(words_01, words_lst, styles, styles_txt)
+
+		print '{}\tnum CM utt: {}'.format(dialog_id, num_cm_utt)
+
+	calc_styles(styles)
+
+	return styles_txt
 
 
 # lbl: list of labels for each tweet
@@ -145,3 +240,204 @@ def parse_twitter(tsvfile):
 		data['txt'].append(txts)
 
 	return data
+
+
+# print CM metrics for Twitter data only
+# return dict where with {style: list(txt examples)}
+def get_style_metrics_twitter(twitter_dict):
+	styles = defaultdict(int)
+	styles_txt = defaultdict(list)
+	num_cm_utt = 0
+	num_total_tweet = 0
+
+	for i, lbl_lst in enumerate(twitter_dict['lbl']):
+		num_total_tweet += 1
+		# convert text labels to list of 0s, 1s
+		words_01 = []
+		for lbl in lbl_lst:
+			if lbl == 'lang1':  # ENG
+				word_01 = 1
+			elif lbl == 'lang2':  # SPA
+				word_01 = 0
+			else:  # other
+				word_01 = 2
+
+			words_01.append(word_01)
+
+		# only process if utt = code-mixed
+		if not (0 in words_01 and 1 in words_01):
+			continue
+
+		num_cm_utt += 1
+		words_lst = twitter_dict['txt'][i]
+		if words_lst[-1] == '':
+			words_lst = words_lst[:-1]
+
+		process_tags(words_01, words_lst, styles, styles_txt)
+
+	print 'num CM utt: {}'.format(num_cm_utt)
+	print 'num total : {}'.format(num_total_tweet)
+	print 'percent CM: {}'.format(float(num_cm_utt)/num_total_tweet)
+
+	calc_styles(styles)
+
+	return styles_txt
+
+
+# multilingual index across all words across users
+def calc_m_idx(chat_lid_lsts):
+	num_spa_eng = [0, 0, 0]  # 3rd spot will be ignored
+	for user, lid_lists in chat_lid_lsts.iteritems():
+		for utt_list in lid_lists:
+			for x in utt_list:
+				num_spa_eng[x] += 1
+
+	# print num_spa_eng
+	num_total_01 = float(num_spa_eng[0] + num_spa_eng[1])
+	sigma = math.pow(num_spa_eng[0]/num_total_01, 2) + math.pow(num_spa_eng[1]/num_total_01, 2)
+	# print 'sigma', sigma
+	return (1 - sigma) / sigma
+
+
+# integration index, averaged per user
+# also print values per user just for sanity check
+def calc_i_idx(chat_lid_lsts):
+	scores = []
+	for user, lid_lists in chat_lid_lsts.iteritems():
+		user_01_list = []
+		for utt_list in lid_lists:
+			user_01_list.extend(utt_list)
+
+		# remove LID tag = 2 (neither eng nor spa)
+		flat_list = [utt for utt in user_01_list if utt < 2]
+
+		if len(flat_list) < 2:
+			score = 0.
+
+		else:
+			num_switches = 0
+			for i, lid in enumerate(flat_list[1:]):
+				if flat_list[i - 1] != lid:
+					num_switches += 1
+
+			score = float(num_switches) / (len(flat_list) - 1)
+
+		scores.append(score)
+
+	# print scores
+	return sum(scores) / len(scores)
+
+
+# print CM metrics for collected COCOA data only
+# return dict where dict[style_label] = {style: list(txt examples)}
+def get_style_metrics_cocoa(filename, chat2style_file):
+
+	# data = defaultdict(list)
+	txt_lst = defaultdict(list)
+	lbl_lst = defaultdict(list)
+	chat2style = {}
+	style2chat = defaultdict(set)
+
+	with open(chat2style_file) as f:
+		for line in f.readlines():
+			chat_id, chat_style = line.strip().split('\t')
+			chat2style[chat_id] = chat_style
+
+	with open(filename) as f:
+		for line in f.readlines():
+			all_info = line.replace('\n', '').split('\t')
+
+			chat_id_concat = '{}_{}'.format(all_info[0], all_info[1].zfill(2))
+			# ex. style2chat['en_lex'].append('chat_id_4')
+			style2chat[chat2style[all_info[0]]].add(chat_id_concat)
+			txt = all_info[2]  # single token
+
+			lbl = all_info[3]
+			if len(all_info) > 4:
+				if all_info[4] != '':
+					# use manually fixed LID tag instead, if applicable
+					lbl = all_info[4]
+
+			txt_lst[chat_id_concat].append(txt)
+			lbl_lst[chat_id_concat].append(lbl)
+
+	# print sorted(txt_lst.keys())
+
+	# this loop flattens txt_lst and lbl_lst (without chat_id_concat)
+	# for chat_id_concat, txts in txt_lst.iteritems():
+	# 	data['lbl'].append(lbl_lst[chat_id_concat])
+	# 	data['txt'].append(txts)
+
+	styles_txt_dct = {}
+
+	for given_style in style2chat.keys():
+
+		styles = defaultdict(int)
+		styles_txt = defaultdict(list)
+		num_cm_utt = 0
+		num_eng_only = 0
+		num_spa_only = 0
+		num_total_tweet = 0
+		chat_lid_lsts = defaultdict(list)
+
+		print 'STYLE: {}'.format(given_style)
+		print 'num utt in style:', len(style2chat[given_style])
+
+		for chat_id_concat in sorted(style2chat[given_style]):
+			# print chat_id_concat
+		# for i, lbl_lst in enumerate(data['lbl']):
+			utt_lbls = lbl_lst[chat_id_concat]
+			# print len(utt_lbls)
+			# import pdb; pdb.set_trace()
+
+			num_total_tweet += 1
+			# convert text labels to list of 0s, 1s
+			words_01 = [int(utt_lbl) for utt_lbl in utt_lbls]
+			chat_lid_lsts[chat_id_concat[:-3]].append(words_01)
+
+			# only process if utt = code-mixed
+			if not (0 in words_01 and 1 in words_01):
+				if 0 in words_01:
+					num_spa_only += 1
+					# print 'SPA: ', data['txt'][i]
+				elif 1 in words_01:
+					num_eng_only += 1
+					# print 'ENG: ', data['txt'][i]
+				else:
+					# print 'NEITHER: ', data['txt'][i]
+					pass
+				continue
+
+			num_cm_utt += 1
+			# words_lst = data['txt'][i]
+			words_lst = txt_lst[chat_id_concat]
+			if words_lst[-1] == '':
+				words_lst = words_lst[:-1]
+
+			process_tags(words_01, words_lst, styles, styles_txt)
+
+		print 'num CM utt: {}'.format(num_cm_utt)
+		print 'num EN utt: {}'.format(num_eng_only)
+		print 'num SP utt: {}'.format(num_spa_only)
+		print 'num total : {}'.format(num_total_tweet)
+		print 'percent CM: {}'.format(float(num_cm_utt)/num_total_tweet)
+		print 'percent EN: {}'.format(float(num_eng_only)/num_total_tweet)
+		print 'percent SP: {}'.format(float(num_spa_only)/num_total_tweet)
+		print
+
+		calc_styles(styles)
+		styles_txt_dct[given_style] = styles_txt
+
+		# import pdb; pdb.set_trace()
+		print 'M-IDX:', calc_m_idx(chat_lid_lsts)
+		print 'I-IDX:', calc_i_idx(chat_lid_lsts)
+
+		print
+		print '*'*20
+		print
+
+	return styles_txt_dct
+
+# filename = './data/manual_19struct_fixed.tsv'
+# chat2style = './data/chatid2style.txt'
+# cocoa_style_txt = get_style_metrics_cocoa(filename, chat2style)
